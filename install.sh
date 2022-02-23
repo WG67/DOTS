fb519da5f73636c07d792353984478a1005d8852
WG67 <Withergaming67@gmail.com>
2022-02-23 11:55:07 +0000

    Added Dots

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install steam discord fish neofetch vim neovim rhythmbox thunar lutris font-manager polybar bspwm i3 blueman htop nitrogen picom gufw terminator 
+
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
